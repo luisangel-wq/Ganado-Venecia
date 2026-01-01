0d0a3d3261029406c73a7ee59c9052d5e2e49870
Luis Angel <luis.angel@matchmindnet.com>
2025-12-31 21:05:49 -0500

    Fix: Remove photo sections from IA tab and fix JavaScript errors

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ganado-finca-v1';
+const CACHE_NAME = 'ganado-finca-v3';
 const urlsToCache = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', event => {
